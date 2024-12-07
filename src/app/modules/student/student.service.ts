b0ffc8e10180c8dd6993a02c1383944ebfe18243
smsaifurrahman <smsaifur525@gmail.com>
2024-12-07 20:59:09 +0100

    admin and faculty modules are created

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -4,73 +4,86 @@ import mongoose from 'mongoose';
 import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { HttpStatus } from 'http-status-ts';
+import QueryBuilder from '../../builder/QueryBuilder';
+import { studentSearchableFields } from './student.constant';
 
 const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
+  // const queryObj = { ...query };
 
-  const queryObj = { ...query };
- 
+  // const studentSearchableFields = ['email', 'name.firstName', 'presentAddress']
+  // let searchTerm = '';
+  // if (query?.searchTerm) {
+  //   searchTerm = query?.searchTerm as string;
+  // }
 
-  let searchTerm = '';
-  if (query?.searchTerm) {
-    searchTerm = query?.searchTerm as string;
-  }
+  // const searchQuery = Student.find({
+  //   $or: studentSearchableFields.map((field) => ({
+  //     [field]: { $regex: searchTerm, $options: 'i' },
+  //   })),
+  // });
 
-  const searchQuery = Student.find({
-    $or: ['email', 'name.firstName', 'presentAddress'].map((field) => ({
-      [field]: { $regex: searchTerm, $options: 'i' },
-    })),
-  });
+  // // Filtering
+  // const excludeFields = ['searchTerm', 'sort', 'limit','page', 'fields'];
+  // excludeFields.forEach((el) => delete queryObj[el]);
 
-  // Filtering
-  const excludeFields = ['searchTerm', 'sort', 'limit','page', 'fields'];
-  excludeFields.forEach((el) => delete queryObj[el]);
+  // const filterQuery = searchQuery
+  //   .find(queryObj)
+  //   .populate('admissionSemester')
+  //   .populate({
+  //     path: 'academicDepartment',
+  //     populate: 'academicFaculty',
+  //   });
 
-  console.log({query}, {queryObj});
+  //   let sort = '-createdAt';
+  //   if (query.sort) {
+  //     sort = query.sort as string;
+  //   }
 
+  //   const sortQuery = filterQuery.sort(sort);
 
-  const filterQuery = searchQuery
-    .find(queryObj)
-    .populate('admissionSemester')
-    .populate({
+  //   let page = 1
+  //   let limit = 1;
+  //   let skip = 0;
+
+  //   if (query.limit) {
+  //     limit =Number( query.limit);
+  //   }
+
+  //   if(query.page) {
+  //     page = Number(query.page);
+  //     skip = (page-1) * limit;
+  //   }
+
+  //   const paginateQuery = sortQuery.skip(skip)
+
+  //   const limitQuery =  paginateQuery.limit(limit);
+
+  //   let fields = '-__v';
+  //   if(query.fields) {
+  //     fields = (query.fields as string).split(',').join(' ')
+
+  //   }
+
+  //   const fieldQuery = await limitQuery.select(fields);
+  //   return fieldQuery;
+  // };
+
+  const studentQuery = new QueryBuilder(
+    Student.find().populate('admissionSemester').populate({
       path: 'academicDepartment',
       populate: 'academicFaculty',
-    });
+    }),
 
-  let sort = '-createdAt';
-  if (query.sort) {
-    sort = query.sort as string;
-  }
+    query,
+  )
+    .search(studentSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
 
-  const sortQuery = filterQuery.sort(sort);
-
-  let page = 1
-  let limit = 1;
-  let skip = 0;
-
-  
-  if (query.limit) {
-    limit =Number( query.limit);
-  }
-
-
-  if(query.page) {
-    page = Number(query.page);
-    skip = (page-1) * limit;
-  }
-
-  const paginateQuery = sortQuery.skip(skip)
-
-
-  const limitQuery =  paginateQuery.limit(limit);
-
-  let fields = '-__v';
-  if(query.fields) {
-    fields = (query.fields as string).split(',').join(' ')
-  
-  }
-
-  const fieldQuery = await limitQuery.select(fields);
-  return fieldQuery;
+  const result = await studentQuery.modelQuery;
+  return result;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
@@ -106,7 +119,7 @@ const updateStudentFromDB = async (id: string, payload: Partial<TStudent>) => {
       modifiedUpdatedData[`localGuardian.${key}`] = value;
     }
   }
-  
+
   const result = await Student.findOneAndUpdate({ id }, modifiedUpdatedData, {
     new: true,
     runValidators: true,
